8807973ed2e505c0b046138604f2da02e5200ada
robjuz <robjuz@gmail.com>
2016-11-11 22:03:11 +0100

    initial commit

---
+++ app/Http/Controllers/PatchworkController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Patchwork
+
+class PatchworkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Patchwork::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $patchwork = Patchwork::find($id);
+        return response()->json($patchwork);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $patchwork = Patchwork::find($id);
+        $patchwork->fill($request->all());
+        $patchwork->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Patchwork::find($id)->delete();
+    }
+}
---
+++ resources/views/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html lang="pl">
+<head>
+	<meta charset="utf-8">
+	<title>
+		Patchworkarnia
+	</title>
+
+	 <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+
+<header>
+	<div id="logo">
+		<a>
+			<img src="logo.jpg" alt="Patchworkarnia">
+		</a>
+	</div>
+	<div id="menu">
+		<div class="menuitem active">
+			<a>
+				Projektowanie
+			</a>
+		</div>
+		<div class="menuitem">
+			<a>
+				Galeria
+			</a>
+		</div>
+		<div class="menuitem">
+			<a>
+				Księga gości
+			</a>
+		</div>
+		<div class="menuitem">
+			<a>
+				Twórcy
+			</a>
+		</div>
+	</div>
+</header>
+
+<main>
+
+	<div class="sidebar-left">
+	</div>
+
+	<div class="patchwork">
+	</div>
+
+</main>
+
+<footer>
+	<span>
+		Patchworkarnia 2016
+	</span>
+	<script src="/js/angular.min.js"
+</footer>
+
+</body>
+</html>
