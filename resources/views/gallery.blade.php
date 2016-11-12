6e0dfa302e44fa5038ce55ed44e8f350b3b1ddf3
Katarzyna Brodzka <katarzyna.b@peac.me>
2016-11-12 18:34:09 +0100

    new gallery 	geändert:               public/css/main.css 	geändert:               resources/views/gallery.blade.php

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -2,5 +2,61 @@
 
 @section('content')
 
+<div id="gallery">
+	<div class="single-patchwork">
+		<div class="title">Pierwszy patchwork</div>
+		<div class="date">dodano: <span>12.11.2016</span></div>
+		<img src="patchwork1.jpg" alt="patchwork1">
+		<div class="statistics">
+			<div class="views">3</div>
+			<div class="likes">2</div>
+		</div>	
+		<a>Szczegóły</a>		
+	</div>
+	
+	<div class="single-patchwork">
+		<div class="title">Kolorowa fantazja</div>
+		<div class="date">dodano: <span>12.11.2016</span></div>
+		<img src="patchwork2.jpg" alt="patchwork1">
+		<div class="statistics">
+			<div class="views">3</div>
+			<div class="likes">2</div>
+		</div>
+		<a>Szczegóły</a>		
+	</div>
+	
+	<div class="single-patchwork">
+		<div class="title">Impresja zimowa</div>
+		<div class="date">dodano: <span>12.11.2016</span></div>
+		<img src="patchwork3.jpg" alt="patchwork1">
+		<div class="statistics">
+			<div class="views">3</div>
+			<div class="likes">2</div>
+		</div>	
+		<a>Szczegóły</a>		
+	</div>
+	
+	<div class="single-patchwork">
+		<div class="title">Inspiracja kodem Morse'a</div>
+		<div class="date">dodano: <span>12.11.2016</span></div>
+		<img src="patchwork4.jpg" alt="patchwork1">
+		<div class="statistics">		
+			<div class="views">3</div>
+			<div class="likes">2</div>
+		</div>	
+		<a>Szczegóły</a>		
+	</div>
+	
+	<div class="single-patchwork">
+		<div class="title">Coś dla początkujących</div>
+		<div class="date">dodano: <span>12.11.2016</span></div>
+		<img src="patchwork1.jpg" alt="patchwork1">
+		<div class="statistics">		
+			<div class="views">3</div>
+			<div class="likes">2</div>
+		</div>
+		<a>Szczegóły</a>		
+	</div>
+</div>
 
 @endsection()
